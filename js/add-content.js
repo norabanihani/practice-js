3d2e20db2a5d2b65df0f0d2f2b913cf542a4899d
norabanihani <norabanihani1994@gmail.com>
2020-03-05 10:11:14 +0200

    new changes

--- js/add-content.js
+++ js/add-content.js
@@ -1,25 +1,37 @@
-var txt;
-var r = confirm("Press a button!");
-if (r == true) {
-  txt = "You pressed OK!";
-  getTime();
-  console.log(txt);
-} else {
-  txt = "You pressed Cancel!";
-  console.log(txt);
+function  createGreating() {
+    
 
-}
-function getTime(){var today= new Date();
-var hourNow = today.getHours();
+
+//var today= new Date();
+var hourNow = prompt('enter the time now');
 var greeting;
-if (hourNow > 18) {
+if (hourNow > 18 && hourNow<24) {
 greeting= 'Good evening!';
-}else if (hourNow > 12) {
+}else if (hourNow > 12 && hourNow <18) {
 greeting = ' Good afternoon!';
-}else if (hourNow > 0) {
-greeting = 'Good morni ng!';
+}else if (hourNow > 0 && hourNow<12) {
+greeting = 'Good morning!';
 }else{greeting = 'Welcome! ' ;
 }
+return greeting; 
+}
 
-document .write( ' <h3>' +greeting + ' </ h3>');
+var showOrder = function(){
+    var userOrder = prompt('what would u like to order');
+    var orderImage="";
+    var numOfImgs;
+    while(userOrder !== "house" && userOrder !=="hotel"){
+        var userOrder = prompt('Please house or hotel');
+    }
+    numOfImgs=prompt('How many ?');
+    for (let ctr = 0; ctr < numOfImgs; ctr++) {
+        console.log("index"+ctr);
+            if(userOrder ==="house"){
+        orderImage +='<img src="../images/house.png">';
+    }else if (userOrder === "hotel"){
+        orderImage =orderImage +'<img src="../images/hotel.png">';
+    }
+    }
+
+    return orderImage;
 }
